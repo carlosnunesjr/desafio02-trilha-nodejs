00ac052999f3d43afb99bfb338f7d582d62997b0
carlosnunesjr <carlosnunes@gmail.com>
2022-05-25 19:29:48 +0000

    First commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
+const { version } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +11,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find( user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({"error": "Username doesn't exists."});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  const totalTodos = user.todos?.length;
+  if(!user.pro && (totalTodos && totalTodos == 10) ){
+    return response.status(403).json({"error": "Invalid parameter"});
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find( user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({"error": "Username doesn't exists."});
+  }
+
+  const {id} = request.params;
+
+  const uuidValid = validate(id);
+ 
+  if(!uuidValid){
+    return response.status(400).json({"error": "Invalid parameter"});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({"error": "Not Found"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  const user = users.find( user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({"error": "Username doesn't exists."});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
